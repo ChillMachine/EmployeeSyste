9658283ec871acd5e349ed8e31e81260d87569f9
ChillMachine <egor2321@gmail.com>
2023-10-03 17:40:55 +0300

    create templates, css

--- main/views.py
+++ main/views.py
@@ -1,4 +1,14 @@
+from django.shortcuts import render
 from django.http import HttpResponse
-  
+ 
 def index(request):
-    return HttpResponse("Hello METANIT.COM")
+    return render(request, "index.html")
+
+def person(request):
+    id = request.GET.get('id', '0')
+    data = {'id': id}
+    if id == '0':
+        return render(request, "index.html", context=data)
+    else:
+        
+        return render(request, "person.html", context=data)
