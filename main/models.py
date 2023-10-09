96f5e1fb0b532dbd717040f4f1a2a3e3275bb66d
ChillMachine <egor2321@gmail.com>
2023-10-09 23:39:25 +0300

    another one fix

--- main/models.py
+++ main/models.py
@@ -19,7 +19,7 @@ class Employee(models.Model):
     personal_num = models.CharField(max_length=4)
     phone_number = models.CharField(max_length=12)
     address = models.CharField(max_length=20)
-    group = models.CharField(max_length=10)
+    group_num = models.CharField(max_length=10)
     family_status = models.CharField(max_length=10)
     place_of_bd = models.CharField(max_length=20)
     appointment_order_num = models.CharField(max_length=10)
