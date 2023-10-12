a3a5d9b815e3e2a7a781daca91cf785fb51185c4
ChillMachine <egor2321@gmail.com>
2023-10-12 22:05:16 +0300

    commit

--- main/static/js/scripts.js
+++ main/static/js/scripts.js
@@ -1,13 +1,10 @@
-
 var currentColumn = -1;
 var ascending = true;
 
-
-function sortTable(columnIndex) {
-    var table, rows, switching, i, x, y, shouldSwitch;
+function sortNumberColumn(columnIndex) {
+    var table, rows, switching, i, shouldSwitch;
     table = document.getElementById("my_table");
     switching = true;
-
 
     ascending = (currentColumn === columnIndex && ascending) ? false : true;
     currentColumn = columnIndex;
@@ -18,12 +15,57 @@ function sortTable(columnIndex) {
 
         for (i = 1; i < rows.length - 1; i++) {
             shouldSwitch = false;
-            x = rows[i].getElementsByTagName("td")[columnIndex].textContent.toLowerCase();
-            y = rows[i + 1].getElementsByTagName("td")[columnIndex].textContent.toLowerCase();
+            var x = parseFloat(rows[i].getElementsByTagName("td")[columnIndex].textContent);
+            var y = parseFloat(rows[i + 1].getElementsByTagName("td")[columnIndex].textContent);
 
-            if ((ascending && x > y) || (!ascending && x < y)) {
-                shouldSwitch = true;
-                break;
+            if (ascending) {
+                if (x > y) {
+                    shouldSwitch = true;
+                    break;
+                }
+            } else {
+                if (x < y) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+}
+
+// Функция для сортировки строк
+function sortStringColumn(columnIndex) {
+    var table, rows, switching, i, shouldSwitch;
+    table = document.getElementById("my_table");
+    switching = true;
+
+    ascending = (currentColumn === columnIndex && ascending) ? false : true;
+    currentColumn = columnIndex;
+
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+
+        for (i = 1; i < rows.length - 1; i++) {
+            shouldSwitch = false;
+            var x = rows[i].getElementsByTagName("td")[columnIndex].textContent.toLowerCase();
+            var y = rows[i + 1].getElementsByTagName("td")[columnIndex].textContent.toLowerCase();
+
+            if (ascending) {
+                if (x > y) {
+                    shouldSwitch = true;
+                    break;
+                }
+            } else {
+                if (x < y) {
+                    shouldSwitch = true;
+                    break;
+                }
             }
         }
 
